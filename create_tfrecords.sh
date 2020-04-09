a5d32e99b115dd4b6e8c2fcd69150e93a17f02e7
areeberg <areeberg@gmail.com>
2020-04-09 12:11:08 -0300

    updated to colab

--- create_tfrecords.sh
+++ create_tfrecords.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+#PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
+#    python3 create_kitti_tf_record.py \
+#            --data_dir=egohands_kitti_formatted \
+#            --output_path=data/egohands \
+#            --classes_to_use=hand \
+#            --label_map_path=data/egohands_label_map.pbtxt \
+#            --validation_set_size=500
+
 PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
-    python3 create_kitti_tf_record.py \
+    python3 /content/hand-detection-tutorial/create_kitti_tf_record.py \
             --data_dir=egohands_kitti_formatted \
-            --output_path=data/egohands \
+            --output_path=/content/hand-detection-tutorial/data/egohands \
             --classes_to_use=hand \
-            --label_map_path=data/egohands_label_map.pbtxt \
+            --label_map_path=/content/hand-detection-tutorial/data/egohands_label_map.pbtxt \
             --validation_set_size=500
+
--- export.sh
+++ export.sh
@@ -50,9 +50,10 @@ OUTPUT_DIR=model_exported
 # clear old exported model
 rm -rf ${OUTPUT_DIR}
 
-PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
-    python3 ./models/research/object_detection/export_inference_graph.py \
+PYTHONPATH=`pwd`/content/models/research/:/content/models/research/slim \
+    python3 /content/models/research/object_detection/export_inference_graph.py \
             --input_type=image_tensor \
             --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
             --trained_checkpoint_prefix=${CHECKPOINT_PREFIX} \
             --output_directory=${OUTPUT_DIR}
+
